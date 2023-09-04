82ffc11783a8687c160c3129bb92658cd8dad376
Property404 <git@dagans.dev>
2023-09-04 12:33:45 -0500

    Allow editing

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
+use anyhow::Result;
 use clap::Parser;
 use std::{
-    env, fs,
+    env,
+    ffi::OsStr,
+    fs,
+    io::{self, Write},
     path::{Path, PathBuf},
+    process::Command,
     sync::OnceLock,
 };
 
@@ -16,6 +21,19 @@ fn notes_dir() -> &'static Path {
         .as_ref()
 }
 
+fn edit_file(path: impl AsRef<OsStr>) -> Result<()> {
+    static EDITOR: OnceLock<(String, Vec<String>)> = OnceLock::new();
+    let (editor, args) = &EDITOR.get_or_init(|| {
+        let args = env::var("EDITOR").expect("$EDITOR not set");
+        let mut args = args.split_whitespace().map(String::from);
+        let editor = args.next().expect("$EDITOR is blank");
+        let args = args.collect();
+        (editor, args)
+    });
+    Command::new(editor).args(&**args).arg(&path).status()?;
+    Ok(())
+}
+
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
 struct Cli {
@@ -23,13 +41,33 @@ struct Cli {
     note: String,
 }
 
-fn main() {
+fn main() -> Result<()> {
     let cli = Cli::parse();
 
-    let note_path = notes_dir().join(cli.note);
+    let note_path = notes_dir().join(&cli.note);
 
-    println!("{note_path:?}");
-    if note_path.exists() {
-        println!("Exists!");
+    if !note_path.exists() {
+        println!("Note '{}' doesn't exist.", cli.note);
+        print!("Would you like to create it(y/n)?");
+        io::stdout().flush()?;
+
+        let mut option = String::new();
+        loop {
+            io::stdin()
+                .read_line(&mut option)
+                .expect("Failed to read from stdin");
+            let option = option.to_lowercase();
+            let option = option.trim();
+
+            if option.starts_with('y') {
+                break;
+            } else if option.starts_with('n') {
+                return Ok(());
+            }
+        }
     }
+
+    edit_file(&note_path)?;
+
+    Ok(())
 }
