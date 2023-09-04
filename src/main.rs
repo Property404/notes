c9da08503cf0b67f2191cdcefbb5a6563c8f5aa5
Property404 <git@dagans.dev>
2023-09-04 13:09:15 -0500

    Allow selecting notes

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 use anyhow::Result;
 use clap::Parser;
 use std::{
+    cmp::Ordering,
     env,
     ffi::OsStr,
     fs,
     io::{self, Write},
+    os::unix::fs::MetadataExt,
     path::{Path, PathBuf},
     process::Command,
     sync::OnceLock,
@@ -38,36 +40,84 @@ fn edit_file(path: impl AsRef<OsStr>) -> Result<()> {
 #[command(author, version, about, long_about = None)]
 struct Cli {
     /// A note to view or edit
-    note: String,
+    note: Option<String>,
 }
 
 fn main() -> Result<()> {
     let cli = Cli::parse();
+    let notes_dir = notes_dir();
 
-    let note_path = notes_dir().join(&cli.note);
+    if let Some(note) = &cli.note {
+        let note_path = notes_dir.join(note);
 
-    if !note_path.exists() {
-        println!("Note '{}' doesn't exist.", cli.note);
-        print!("Would you like to create it(y/n)?");
-        io::stdout().flush()?;
+        if !note_path.exists() {
+            println!("Note '{note}' doesn't exist.");
+            print!("Would you like to create it(y/n)?");
+            io::stdout().flush()?;
+
+            let mut option = String::new();
+            loop {
+                io::stdin()
+                    .read_line(&mut option)
+                    .expect("Failed to read from stdin");
+                let option = option.to_lowercase();
+                let option = option.trim();
+
+                if option.starts_with('y') {
+                    break;
+                } else if option.starts_with('n') {
+                    return Ok(());
+                }
+            }
+        }
+
+        edit_file(&note_path)?;
+    } else {
+        let notes: io::Result<Vec<_>> = fs::read_dir(notes_dir)?.collect();
+        let mut notes = notes?;
+
+        // Sort by access time
+        notes.sort_unstable_by(|a, b| {
+            a.metadata()
+                .map(|m| m.atime())
+                .unwrap_or_default()
+                .partial_cmp(&b.metadata().map(|m| m.atime()).unwrap_or_default())
+                .unwrap_or(Ordering::Equal)
+        });
+
+        if notes.is_empty() {
+            println!("Could not find any notes");
+            return Ok(());
+        }
+
+        println!("Found the following:");
+        println!("====================");
+        for (index, note) in notes.iter().enumerate() {
+            let note = note.file_name();
+            let note = note.to_str().expect("Encountered bad unicode in file name");
+            println!("{index}. {note}");
+        }
 
         let mut option = String::new();
         loop {
-            io::stdin()
-                .read_line(&mut option)
-                .expect("Failed to read from stdin");
-            let option = option.to_lowercase();
+            print!("Select> ");
+            io::stdout().flush()?;
+            io::stdin().read_line(&mut option)?;
             let option = option.trim();
+            let option_lowercase = option.to_lowercase();
 
-            if option.starts_with('y') {
+            if option_lowercase == "q" || option_lowercase == "quit" {
                 break;
-            } else if option.starts_with('n') {
-                return Ok(());
+            }
+
+            if let Ok(number) = option.parse::<usize>() {
+                if number < notes.len() {
+                    edit_file(notes[number].path())?;
+                    break;
+                }
             }
         }
     }
-
-    edit_file(&note_path)?;
 
     Ok(())
 }
