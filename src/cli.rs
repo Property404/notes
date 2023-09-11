a5505c6f43df98b32271ee99edad67adb932d464
Property404 <git@dagans.dev>
2023-09-11 13:06:55 -0500

    Add argument conflicts

--- src/cli.rs
+++ src/cli.rs
@@ -15,24 +15,24 @@ pub enum SortBy {
 #[command(author, about, long_about = None, version)]
 pub struct Cli {
     /// Execute git command
-    #[clap(long, allow_hyphen_values=true, num_args = 1..,value_name="ARGS")]
+    #[clap(long, allow_hyphen_values=true, num_args = 1..,value_name="ARGS", group="main")]
     pub git: Option<Vec<String>>,
 
     /// Execute ripgrep command
-    #[clap(long, allow_hyphen_values=true, num_args = 1..,value_name="ARGS")]
+    #[clap(long, allow_hyphen_values=true, num_args = 1..,value_name="ARGS", group="main")]
     pub rg: Option<Vec<String>>,
 
     /// Execute a command in the notes directory. `{}` will be expanded to all note files as a
     /// separate arg
-    #[clap(long, allow_hyphen_values=true, num_args = 1..,value_name="ARGS")]
+    #[clap(long, allow_hyphen_values=true, num_args = 1..,value_name="ARGS", group="main")]
     pub exec: Option<Vec<String>>,
 
     /// Get directory of note
-    #[clap(long, value_name = "NOTE")]
+    #[clap(long, value_name = "NOTE", group = "main")]
     pub dir: Option<Option<String>>,
 
     /// List notes
-    #[clap(long)]
+    #[clap(long, group = "main")]
     pub list: bool,
 
     /// Set sorting method
@@ -40,10 +40,10 @@ pub struct Cli {
     pub sort_by: Option<SortBy>,
 
     /// Remove note
-    #[clap(long, value_name = "NOTE", alias = "rm")]
+    #[clap(long, value_name = "NOTE", alias = "rm", group = "main")]
     pub remove: Option<String>,
 
     /// A note to view or edit
-    #[clap(value_hint = ValueHint::Unknown)]
+    #[clap(value_hint = ValueHint::Unknown, group = "main")]
     pub note: Option<String>,
 }
